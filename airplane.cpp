24ccf8f3384e9fd1ad5912b42925f8fd00c85841
Aayush Shrivastav <32818463+aayushs879@users.noreply.github.com>
2019-08-05 00:45:37 +0530

    Update airplane.cpp
    added comments

--- airplane.cpp
+++ airplane.cpp
@@ -1,10 +1,10 @@
 #include <iostream>
 using namespace std;
-int mat[12][5];
+int mat[12][5]; //declare as a global variable
 int n; //no. of rows
-int det[5][5];
+int det[5][5]; // we're gonna detonate each and every row and undetonate it to check all the cases
 
-
+//this function stores the detonated bomnbs' location in a mirror order manner
 void detonate(int r){
     for (int i = r; i > r-5 && i >=0; i--)
         for (int j =0; j < 5; j++){
@@ -16,6 +16,7 @@ void detonate(int r){
         }
 }
 
+//the undetonate function
 void undet(int r){
     for (int i = r; i >r-5 && i >= 0; i--)
         for (int j = 0; j <5 ; j++){
@@ -25,22 +26,28 @@ void undet(int r){
         }
 }
 
+
+//exhaust all moves to see what works
 void go (int n, int pos, int c, int & ans){
-    if (pos < 0 || pos >4 || c ==-1){
+    if (pos < 0 || pos >4 || c ==-1){ // c= -1 condition is important as we're not gonna go ahead if the coins become -1 at any instant of time
         return;
     }
+
+    //the shit below is simple enough
     if (mat[n -1][pos] == 1){
         c +=1;
     }
     else if (mat[n-1][pos] == 2){
         c -=1;
     }
-
+    //if we have gone through the last row
     if (n == 1){
         if (c >ans){
             ans = c;
         }
     }
+
+    //checking for all the moves; recursion is a bitch
     else{
         go(n -1, pos , c, ans);
         go(n -1, pos +1 , c, ans);
