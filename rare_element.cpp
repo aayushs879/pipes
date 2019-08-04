4a59c60a7aedc47089cb878f5d065cced19fc7af
Aayush Shrivastav <32818463+aayushs879@users.noreply.github.com>
2019-08-05 04:45:09 +0530

    uploaded the original code

--- rare_element.cpp
+++ rare_element.cpp
@@ -20,46 +20,50 @@ below is a sample input for the code
 
 #include <iostream>
 using namespace std;
-int mat[21][21];
-int N, C;
+int mat[20][20];
+int visited[4][20][20];
+int C;
+int N;
 int rare[4][2];
-bool visited[4][21][21];
 
 struct node{
     int x; 
     int y;
-    node (int a , int b){
-        x = a; 
+    node(int a, int b){
+        x = a;
         y = b;
     }
     node(){}
+
 };
 
 struct ll{
     node data;
-    ll * next;
-    ll(){
+    ll* next;
+    ll(node d){
+        data = d;
         next = NULL;
     }
-    ll(node d){
-        data = d; 
+    ll(){
         next = NULL;
     }
 };
 
 class queue{
+    ll* front;
+    ll* back;
     int size;
-    ll * front;
-    ll * rear;
     public:
     queue(){
-        size = 0;
-        front = rear = NULL;
+        size  = 0;
+        front = NULL;
+        back = NULL;
     }
+    bool isEmpty();
     void push(int a, int b);
     node pop();
-    bool isEmpty();
-}
+    
+};
 
 bool queue::isEmpty(){
     if (size == 0){
@@ -67,65 +71,133 @@ bool queue::isEmpty(){
     }
     return false;
 }
+
 void queue::push(int a, int b){
-    if(isEmpty()){
-        node d(a, b);
+    if (isEmpty()){
+        node d;
+        d.x = a;
+        d.y = b;
         ll * temp;
         temp = new ll(d);
-        front = rear = temp;
+        front = temp;
+        back = temp;
     }
     else{
-        node(a, b);
-        ll *temp;
+        node d;
+        d.x = a;
+        d.y = b;
+        ll * temp;
         temp = new ll(d);
-        back = back-> next;
-   }
-   size ++;
+        back -> next = temp;
+        back = back -> next;
+    }
+    size ++;
 }
-node queue::pop(){
+node queue:: pop(){
     node d;
-    d = front->data;
-    size--;
-    front = front ->next;
+    d = front ->data;
+    ll*temp = front -> next;
+    free(front);
+    front = temp;
+    size --;
     return d;
 }
 
+bool safe(int x, int y){
+    if (mat[x][y] != 1 || x>= N|| x<0 || y >= N || y < 0){
+        return false;
 
-void solve(int n, int x, int y){
-    queue p;
-
+    }
+    return true;
 }
 
-
-
-void go(){
-    for(int i = 0; i , C; i++){
-        cin>>rare[i][0]>>rare[i][1];
-        rare[i][0]--;
-        rare[i][1]--; //since the co- odinates are based on indexing starting from 1
-    }
-    for (int i =0 ; i < N; i++){
-        for (int i =0 ; i < N; i++){
-            cin>>mat[i][j];
-            for (int k = 0 ; k <=3; k++){
-                visited[k][i][j] = -1;
-            }
+void go(int n, int x, int y){
+    queue q;
+    q.push(x, y);
+    visited[n][x][y] = 0;
+    while (!q.isEmpty()){
+        node d = q.pop();
+        x = d.x;
+        y = d.y;
+        if (safe(x+1, y) && visited[n][x+1][y] == -1){
+            visited[n][x+1][y] = visited[n][x][y] + 1;
+            q.push(x+1, y);
+        }
+        if (safe(x-1, y) && visited[n][x-1][y] == -1){
+            visited[n][x-1][y] = visited[n][x][y] + 1;
+            q.push(x-1, y);
+        }
+        if (safe(x, y+1) && visited[n][x][y+1] == -1){
+            visited[n][x][y+1] = visited[n][x][y] + 1;
+            q.push(x, y+1);
+        }
+        if (safe(x, y-1) && visited[n][x][y-1] == -1){
+            visited[n][x][y-1] = visited[n][x][y] + 1;
+            q.push(x, y-1);
         }
     }
-    for (int i =0 ; i<C; i++){
-        solve(i, rare[i][0], rare[i][1]) //this is nothing but bfs for each rare element wherein rare[i][j] are co-ordinates of the rare elements
+
+}
+
+
+
+
+void solve(){
+    for (int i = 0; i < C; i++){
+        go (i, rare[i][0], rare[i][1]);
+    }
+
+    int overall = 1e7;
+    int bigshort = -1; 
+    for (int i = 0 ; i <N; i++){
+        for (int j = 0; j < N; j++){
+
+            for (int k = 0, sum = 0, maxi = 0  ; k < C; k++){
+                if (visited[k][i][j] == -1){
+                    break;
+                }
+                sum += visited[k][i][j];
+                maxi = max(maxi, visited[k][i][j]);
+                if(k == C-1){
+                    if (sum < overall){
+                        overall = sum;
+                        bigshort = maxi;
+                    }
+                }
+            }
+            
+        }
 
     }
+
+    cout << bigshort;
 }
+
+
+
+
 
 int main(){
     int t;
-    while (t--){
+    cin>>t;
+    while(t--){
         cin>>N>>C;
-        go();
+        for (int i = 0; i < C; i++){
+            cin>>rare[i][0]>>rare[i][1];
+            rare[i][0]--;
+            rare[i][1]--;
+        }
+        for (int i = 0 ; i < N ; i ++){
+            for(int j = 0; j < N; j++){
+                cin>>mat[i][j];
+                for (int k = 0; k < C ; k++){
+                visited[k][i][j] = -1;
+                }
+            }
+        }
+        solve();
     }
 
-
-
-    return 0;
 }
+
+
